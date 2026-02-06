78310c748f46d7d60c4ccb16007e8356bd7be82d
wangsai <15102045545@163.com>
2026-02-06 18:00:26 +0800

    v0.0.2

--- composeApp/src/androidMain/kotlin/com/example/xiaokeer_app/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/xiaokeer_app/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!!!")
+                Text("Click me! v0.0.2")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
